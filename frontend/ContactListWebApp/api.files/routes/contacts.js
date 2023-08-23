5fcb6958fd4daea92f43529e50a0607ddc75f05c
moataz-afifi <116615953+moataz-afifi@users.noreply.github.com>
2023-08-23 15:28:31 +1000

    Update contacts.js

--- frontend/ContactListWebApp/api.files/routes/contacts.js
+++ frontend/ContactListWebApp/api.files/routes/contacts.js
@@ -91,6 +91,16 @@ async function openRealm() {
       sync: {
         user: app.currentUser,
         flexible: true,
+        clientReset: {
+          mode: "discardUnsyncedChanges",
+          onBefore: (realm) => {
+            console.log("Beginning client reset for ", realm.path);
+          },
+          onAfter: (beforeRealm, afterRealm) => {
+            console.log("Finished client reset for", beforeRealm.path);
+            console.log("New realm path", afterRealm.path);
+          },
+        },
         initialSubscriptions: {
           update: (subs, realm) => {
             subs.add(
